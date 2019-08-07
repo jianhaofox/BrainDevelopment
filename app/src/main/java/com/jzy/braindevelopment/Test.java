11142e84a00e16a20b7417d0dfa8b60c36309da3
jzk603 <123456>
2019-08-07 17:57:19 +0800

    第一次整合 主页 登录 标题栏

--- app/src/main/java/com/jzy/braindevelopment/Test.java
+++ app/src/main/java/com/jzy/braindevelopment/Test.java
@@ -1,9 +1,13 @@
 package com.jzy.braindevelopment;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.jzy.braindevelopment.comment.InitActivity;
+import com.jzy.braindevelopment.user.LoginActivity;
 
 public class Test extends AppCompatActivity {
 
@@ -12,5 +16,13 @@ public class Test extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
         InitActivity.initTitle(Test.this,"测试");
+        Button button = (Button)findViewById(R.id.btn_login);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Test.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/jzy/braindevelopment/user/LoginActivity.java
@@ -0,0 +1,17 @@
+package com.jzy.braindevelopment.user;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.jzy.braindevelopment.R;
+import com.jzy.braindevelopment.comment.InitActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        InitActivity.initTitle(this,"登录");
+    }
+}

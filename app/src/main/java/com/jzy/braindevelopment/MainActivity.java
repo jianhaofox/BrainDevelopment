35badc7f05f2e5c0e273bcc07304f5501fcfc5b7
TheShy <940414473@qq.com>
2019-08-08 15:21:50 +0800

    登陆页面初步完成

--- app/src/main/java/com/jzy/braindevelopment/MainActivity.java
+++ app/src/main/java/com/jzy/braindevelopment/MainActivity.java
@@ -5,7 +5,9 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.annotation.NonNull;
+import android.text.InputType;
 import android.view.MenuItem;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.jzy.braindevelopment.comment.InitActivity;
--- app/src/main/java/com/jzy/braindevelopment/user/LoginActivity.java
+++ app/src/main/java/com/jzy/braindevelopment/user/LoginActivity.java
@@ -13,5 +13,6 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         InitActivity.initTitle(this,"登录");
+
     }
 }
